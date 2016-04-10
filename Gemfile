aa212d060439238523a5a5ea8bfa082375f3c7f5
Moe Alkhafaji <mo.alkhafaji@gmail.com>
2016-04-09 17:29:11 -0700

    fixing issues

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :development do
   gem 'capistrano-sidekiq'
   gem 'capistrano-passenger'
   gem 'net-ssh', '~>3.1'
+  gem 'sidekiq'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :use_sudo, false
 
 # Default value for :linked_files is []
 #set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml')
-set :linked_files, fetch(:linked_files, []).push('config/application.yml')
+# set :linked_files, fetch(:linked_files, []).push('config/application.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
