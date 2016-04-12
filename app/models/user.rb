1008f58c247b1d0f66af5bc2e9f7d5e693d4b8eb
Moe Alkhafaji <mo.alkhafaji@gmail.com>
2016-04-12 11:45:19 -0700

    I had the wrong name for accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation,
+  attr_accessor :first_name, :last_name, :email, :password, :password_confirmation,
                   :temp_password
 
   validates :email, :first_name, :last_name, :presence =>true
